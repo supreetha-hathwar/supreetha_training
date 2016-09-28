25105a67d7ea934348d5fc8b7345f6d9625ab142
supreetha <smahabaleshwara@qwinix.io>
2016-09-28 16:59:07 +0530

    28-sep-2016

---
+++ back-end/RUBY/file.rb
@@ -0,0 +1,16 @@
+class Person
+	a="hi"
+	b="hello"
+	puts "#{a}"
+	# puts " "
+	putc "#{b}"
+	print "qwinix"
+	# puts "enter"
+	# d=gets
+	# puts d
+    # puts "add"
+    a=gets.to_i
+    b=gets.to_i
+
+    print a+b
+end
